4b0121a936784934a94d0e9475d12cb4e2e3766e
Rulo Kobashikawa <akobashikawa@gmail.com>
2021-03-19 22:09:58 -0500

    Usa api en https

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const Component = {
-  API_BASE_URL: 'http://rulokoba.me:3000/api/misc',
+  API_BASE_URL: 'https://rulokoba.me/api/misc',
   GOOGLE_SHEET_ID: '1tWUj9J-DxL0c89FgRmUPHQutLETA0oXlCPrjtQYpa0s',
 };
 
